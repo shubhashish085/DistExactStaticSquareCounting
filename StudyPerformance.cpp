240f356f547e597fd7be5a264dfbe46b2fd3fbd1
Antu <shubhashishkar@gmail.com>
2026-06-20 00:22:36 -0700

    [add] distributed counting for bidirectional graph added

--- StudyPerformance.cpp
+++ StudyPerformance.cpp
@@ -144,6 +144,26 @@ int main(int argc, char** argv){
     MPI_Finalize();
 }*/
 
+//Bidirectional Edge 
+/*int main(int argc, char** argv){
+
+    MPI_Init(&argc, &argv);
+
+    std::string input_data_graph_file = argv[1];
+    std::string vertex_partition_file = argv[2];
+    std::string partition_count = argv[3];
+
+    int partition_cnt = std::stoi(partition_count);
+
+    std::cout << "Input Graph File : " << input_data_graph_file << std::endl;
+    std::cout << "Vertex Partition File : " << vertex_partition_file << std::endl;
+    std::cout << "Partition Count : " << partition_count << std::endl;
+
+    DistributedCountingAlgorithm::db_count_square_with_interface_graph_optimization_in_bi_edges_detail(input_data_graph_file, vertex_partition_file, partition_cnt);
+
+    MPI_Finalize();
+}*/
+
 //Analysis
 /*int main(int argc, char** argv){
 
--- distributedcountingalgo.h
+++ distributedcountingalgo.h
@@ -14,6 +14,7 @@ public:
     static void db_count_square_with_interface_graph_optimization_detail(const std::string& file_path, const std::string& vertex_partition_file_path, int partition_count); 
 
     static void optimized_db_count_square_in_whole_graph_bidirection_edges_parallel(const std::string& file_path, const std::string& vertex_partition_file_path, int partition_count); 
+    static void db_count_square_with_interface_graph_optimization_in_bi_edges_detail(const std::string& file_path, const std::string& vertex_partition_file_path, int partition_count); 
 };
 
 
